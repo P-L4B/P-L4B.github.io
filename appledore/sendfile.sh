04647474c6fd65aff2b82ef9791795532dd6a1a9
P-LAB <1200n81@gmail.com>
2022-10-31 09:31:32 +0100

    added Appledore

---
+++ appledore/sendfile.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#Send file to device character by character. Optimized for Apple-1
+#Change device file if necessary
+DEVICE="/dev/ttyUSB0"
+stty -F $DEVICE 9600 cs8 -cstopb -parenb
+INPUT=$1
+while IFS= read -r -n1 char
+	do
+	if [ "$char" = "" ];
+		then
+		echo -n -e '\x8d' > $DEVICE
+		sleep 0.5
+		echo
+	fi
+	echo -n "$char" > $DEVICE
+	echo -n "$char"
+	sleep 0.06
+done < "$INPUT"
+exit 0
